770d22d7e9ad04e4785b2275b7da18434eec4adb
Damir Sarkulin <sarkulindamir@gmail.com>
2021-04-27 03:55:10 +0500

    work in process (create order)

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\v1\OrderController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,9 +23,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('login', [AuthController::class, 'signin']);
 Route::post('register', [AuthController::class, 'signup']);
 
- Route::middleware('auth:sanctum')->group(function () {
-     Route::post('logout', [AuthController::class, 'logout']);
-     Route::apiResources([
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('orders', [OrderController::class, 'index']);
+    Route::apiResources([
         'drivers' => DriverController::class,
         'cars'    => CarController::class,
         'tariffs' => TariffController::class,
